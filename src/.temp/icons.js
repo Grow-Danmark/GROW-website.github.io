402193148facbfe77b8e0d2bae081749d6a60271
Scott Lind <scttlnd@gmail.com>
2021-06-09 14:40:30 +0200

    Revert "Merge branch 'wp-content-mads' into main"
    This reverts commit c6e388ee2d089fa1ed133d33715e8c16d92f477e, reversing
    changes made to 094f942cb86222ed0a279abe55587bc97aeae6c9.

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=27f6ced"
+      "src": "/assets/static/src/favicon.png?width=76&key=33b30ab"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=27f6ced"
+      "src": "/assets/static/src/favicon.png?width=152&key=33b30ab"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=27f6ced"
+      "src": "/assets/static/src/favicon.png?width=120&key=33b30ab"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=27f6ced"
+      "src": "/assets/static/src/favicon.png?width=167&key=33b30ab"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=27f6ced"
+      "src": "/assets/static/src/favicon.png?width=180&key=33b30ab"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=2fe8a22"
+      "src": "/assets/static/src/favicon.png?width=16&key=9cb5e12"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=2fe8a22"
+      "src": "/assets/static/src/favicon.png?width=32&key=9cb5e12"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=2fe8a22"
+      "src": "/assets/static/src/favicon.png?width=96&key=9cb5e12"
     }
   ]
 }

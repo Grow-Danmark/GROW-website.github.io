30c693cda29ef766e15a5320125f691ab2e03c1c
Scott Lind <scttlnd@gmail.com>
2021-06-10 15:21:21 +0200

    Help component added

--- src/components/HelpCards.vue
+++ src/components/HelpCards.vue
@@ -1,7 +1,27 @@
 <template>
 <section class="help-section">
-  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, voluptate.
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus facilis sunt exercitationem quod corrupti ipsa enim pariatur eaque nisi quos nam vero provident vitae quaerat, officiis ducimus obcaecati unde, laudantium quidem cupiditate. Explicabo impedit quae illum officia voluptatem dolores quas.
+  <div class="help-headers">
+  <h2>Hvordan kan GROW hjælpe dig?</h2>
+  <h3>Mega fed og inspirerende tekst</h3>
+  </div>
+<div class="help-cards">
+  <div class="help-card">
+    <g-image src="~/assets/img/icons/heledig.svg" alt="" class="help-img" quality="100"></g-image>
+    <h4 class="help-text">Plads til hele dig</h4>
+  </div>  
+  <div class="help-card">
+    <g-image src="~/assets/img/icons/balance.svg" alt="" class="help-img" quality="100"></g-image>
+    <h4 class="help-text">Finde balance</h4>
+  </div>  
+  <div class="help-card">
+    <g-image src="~/assets/img/icons/fremdrift.svg" alt="" class="help-img" quality="100"></g-image>
+    <h4 class="help-text">Meningsfuld fremdrift</h4>
+  </div>  
+  <div class="help-card">
+    <g-image src="~/assets/img/icons/nysgerrig.svg" alt="" class="help-img" quality="100"></g-image>
+    <h4 class="help-text">Søge nysgerrigt</h4>
+  </div>
+ </div>
 </section>
 </template>
 
@@ -13,7 +33,38 @@ export default {
 </script>
 
 <style lang="scss">
+@use 'src/assets/sass/abstracts/_variables.scss' as v;
 .help-section {
-    background-color: grey;
+  margin: 0 auto;
+}
+.help-headers {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.help-cards {
+  display: flex;
+  justify-content: space-evenly;
+
+  border: 1px solid black;
+}
+.help-card {
+  display: flex;
+  flex-direction: column;
+  max-width: 10em;
+  flex-basis: 25%;
+
+  @media screen and (max-width: 40em) {
+    margin: .5em;
+  }
+}
+.help-img {
+  width: 80%;
+  margin: 0 auto;
+}
+.help-text {
+  font-size: .8em;
+  text-align: center;
+  margin: 1em auto;
 }
 </style>

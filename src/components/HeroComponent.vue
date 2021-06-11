898305343ad147503df1bb275f603dd63c8cf256
Scott Lind <scttlnd@gmail.com>
2021-06-11 10:07:33 +0200

    Refactor and gitignore

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -6,8 +6,7 @@
     <p>{{ $static.page.heroSection.heroText }}</p>
     </div>
     <div class="blob">
-      <img :src="$static.page.heroSection.blobbg.sourceUrl" :alt="$static.page.heroSection.heroImg.altText" class="hero-image alpha-target">
-      <img :src="$static.page.heroSection.heroImg.sourceUrl" class="blob-bg alpha-target">
+      <img :src="$static.page.heroSection.heroImg.sourceUrl" :alt="$static.page.heroSection.heroImg.altText" class="hero-image alpha-target">
     </div>
 </section>
 </template>
@@ -22,9 +21,6 @@ query hero {
       heroImg {
         altText
         sourceUrl(size: LARGE)
-      }
-      blobbg{
-        sourceUrl(size:LARGE)
       }
     }
   }
@@ -66,25 +62,5 @@ export default {
 
 .hero-text-container > p:nth-child(1){
     color: var(--clr-teal-400); 
-}
-.alpha-target {
-  mask-image: url(../assets/img/blob.svg);
-  mask-mode: alpha;
-  mask-repeat: no-repeat;
-  mask-size: 60%;
-  transform: rotate(35deg);
-}
-.blob {
-  position: relative;
-  fill: grey;
-  transform: rotate(-95deg);
-
-}
-.blob-bg {
-    position: absolute;
-    top: 8em;
-    right: 5em;
-    z-index: 1;
-    transform: rotate(95deg);
 }
 </style>

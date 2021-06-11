41fca982321c8266f523d67fc5a2ab24351e79e3
madsmadkas <1087348@ucn.dk>
2021-06-11 10:21:58 +0200

    Revert "mads fredag"
    This reverts commit a8c5ede2ad8cd0b8471525de09958d57e921723a.

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=27f6ced"
+      "src": "/GROW-website.github.io/assets/static/favicon.f22e9f3.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=27f6ced"
+      "src": "/GROW-website.github.io/assets/static/favicon.62d22cb.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=27f6ced"
+      "src": "/GROW-website.github.io/assets/static/favicon.1539b60.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=27f6ced"
+      "src": "/GROW-website.github.io/assets/static/favicon.dc0cdc5.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=27f6ced"
+      "src": "/GROW-website.github.io/assets/static/favicon.7b22250.954d546a66ffd44ae5467db25eb23279.png"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=2fe8a22"
+      "src": "/GROW-website.github.io/assets/static/favicon.ce0531f.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=2fe8a22"
+      "src": "/GROW-website.github.io/assets/static/favicon.ac8d93a.954d546a66ffd44ae5467db25eb23279.png"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=2fe8a22"
+      "src": "/GROW-website.github.io/assets/static/favicon.b9532cc.954d546a66ffd44ae5467db25eb23279.png"
     }
   ]
 }

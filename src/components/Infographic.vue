34079675f12a17ebd184a46407d208b73294c0de
Scott Lind <scttlnd@gmail.com>
2021-06-15 13:53:58 +0200

    Infographics fixed for mobile

--- src/components/Infographic.vue
+++ src/components/Infographic.vue
@@ -5,7 +5,7 @@
       <p class="subheading">Se på kortet nedenfor, hvor du kan finde dit lokale GROW fælleskab</p>
       </div>
     <div class="infographic">
-        <g-image src="../assets/img/Infographic.png" alt="Danmarkskort som viser hvor GROW du kan finde GROW fælleskaber"></g-image>
+        <g-image src="../assets/img/Infographic.png" alt="Danmarkskort som viser hvor GROW du kan finde GROW fælleskaber" class="info-img"></g-image>
     </div>
 </section>
 </template>
@@ -24,22 +24,28 @@ export default {
 <style lang="scss">
 @use '../assets/sass/abstracts/variables' as v;
 
-
 .info-section {
     margin: 5em auto;
 }
 .infographic {
-    margin: 5em 0;
+    display: flex;
+    margin: 5em;
+    img {
+        max-width: 80%;
+    }
+}
+.info-img {
+    margin: 0 auto;
 }
 
 @include v.mq(medium) {
-    img{
-        width: 30em;
+    .infographic {
+        margin: 1em;
+
+        img {
+            width: 30em;
+        }
     }
 }
-@include v.mq(large) {
-    img{
-        width: 50em;
-    }
-}
+
 </style>

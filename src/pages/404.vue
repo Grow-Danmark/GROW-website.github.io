395dff2d19dbae2827327be89b9a5a68a06cafac
Scott Lind <scttlnd@gmail.com>
2021-06-15 11:07:02 +0200

    Create 404.vue

---
+++ src/pages/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+  <div class="not-found">
+    <h1>Siden blev desværre ikke fundet</h1>
+    <p>Klik <g-link to="/">her</g-link> for at komme til forsiden.</p>
+  </div>
+  </Layout>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  },
+  name: '404',
+  metaInfo() {
+    return {
+      title: 'GROW',
+      titleTemplate: 'GROW' + ' | ' + 'Siden blev ikke fundet',
+      meta: [
+        { name: 'description', content: 'GROW' + ' | ' + 'Siden blev ikke fundet' }
+      ]
+      // etc...
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
